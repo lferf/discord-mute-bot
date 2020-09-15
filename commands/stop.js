cda79b8ca012755fbbd598fbfc5ce10bdf0ba4f1
lFer A <offbeatboy123@gmail.com>
2020-09-14 20:39:38 -0600

    Filter channel by name instead of ID

--- commands/stop.js
+++ commands/stop.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'stop',
   description: 'Stops listening for reactions and cleans the text channel from bot messages',
   execute (message) {
-    if (message.channel.id === '751467327551176905') {
+    if (message.channel.name === 'mute-bot') {
       message.channel.messages.fetch({ limit: 10 })
         .then(messages => message.channel.bulkDelete(messages))
         .catch(console.log('No messages where retrieved.'));
